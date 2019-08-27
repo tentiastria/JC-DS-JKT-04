6b47a7237064da270109169ddf9073630df41921
cornelliusyudhawijaya <32679460+cornelliusyudhawijaya@users.noreply.github.com>
2019-08-27 12:19:11 +0700

    Add files via upload

---
+++ latihan_intro.py
@@ -0,0 +1,45 @@
+# No1
+# x=4
+# y=3
+# z=2
+# w = ((x+y*z)/(x*y))**z
+# print(w)
+
+
+# #No2
+# angka = int(input("Silahkan masukkan angka berapapun: "))
+# print(" Kuadrat dari "+ str(angka) + " = " + str(angka**2))
+
+# No3 (total hari dijadikan satu - satu ke tahun, bulan, etc.)
+# import math
+# total_hari1 = int(input("masukkan hari: "))
+# tahun1 = str(math.floor(total_hari1/360))
+# bulan1 = str(math.floor((total_hari1 % 360)/30))
+# minggu1 = str(math.floor(((total_hari1 % 360)%30)/7))
+# hari1 = str(math.floor(((total_hari1 % 360)%30)%7))
+# print(tahun1 + " tahun " + bulan1 + " bulan " +  minggu1 + " minggu " + hari1 + " hari ")
+
+
+# #No4
+# umur_budi = 490/14
+# umur_andi = 49 - umur_budi
+# print(umur_andi + 2)
+# print(umur_budi + 2)
+
+# No5
+# word = input('Kata: ')
+# cari = input('Karakter yang ingin dicari: ')
+# print(int(len(word.split(cari)))-1)
+
+
+
+# No6
+# jarak_dalam_km = int(input("Jarak antara kendaraan?: "))
+# kecepatan_a_km_per_h = int(input("Kecepatan kendaraan a?: "))
+# kecepatan_b_km_per_h = int(input("Kecepatan kendaraan b?: "))
+
+# waktu_tabrakan_dalam_menit = (
+#     jarak_dalam_km/(kecepatan_a_km_per_h + kecepatan_b_km_per_h))*60
+# print(str(waktu_tabrakan_dalam_menit) + ' menit')
+#jam 10.12 WIB
+
